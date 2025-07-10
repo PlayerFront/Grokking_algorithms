a57efbf8890f03a1175c1cc28a0417095ea160b6
Elena <elena.wictorovna@gmail.com>
2025-07-10 15:18:59 +0300

    1

---
+++ Binary_search/binary_search.js
@@ -0,0 +1,22 @@
+
+function binary_search (arr, item){
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high) {
+        const middle = Math.floor((low + high) / 2);
+        const suppose = arr[middle];
+
+        if (suppose === item) return middle;
+        else if (suppose > item) high = middle - 1;
+        else low = middle + 1;
+    }
+
+    return null;
+}
+
+const arrOfNumbers = [1, 3, 5, 7, 9];
+const arrOfNames = ['Helena', 'Antony', 'Kevin', 'Alex', 'Kate'];
+
+console.log(binary_search(arrOfNumbers, 3));
+console.log(binary_search(arrOfNames, 'Helena'));
